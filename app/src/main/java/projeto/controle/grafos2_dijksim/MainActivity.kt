426ee7a5db971f003bce64d5df39c4f1fed89c1a
MatheusHenrick <matheu81h@gmail.com>
2023-10-12 20:52:19 -0300

    Adicionando a visualização do grafo

---
+++ app/src/main/java/projeto/controle/grafos2_dijksim/MainActivity.kt
@@ -0,0 +1,11 @@
+package projeto.controle.grafos2_dijksim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
